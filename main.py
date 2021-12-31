418a24bb028b511c7842b238cb1f94fa5d2d5794
MrSmIlEyar <94910911+MrSmIlEyar@users.noreply.github.com>
2021-12-31 11:29:02 +0300

    only show bg in start_game func

---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+import time
+import pygame
+import os
+import sys
+
+pygame.init()
+
+size = width, height = 801, 601
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Space Battle")
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def end_game():
+    running = False
+    pygame.quit()
+    sys.exit()
+
+
+def start_game():
+    global running
+    background = load_image('background.png')
+    FPS = 50
+    v = 1
+    clock = pygame.time.Clock()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        screen.blit(background, (0, 0))
+        clock.tick(FPS)
+        where = -1
+        pygame.display.update()
+
+
+running = True
